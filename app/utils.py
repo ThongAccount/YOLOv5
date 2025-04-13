f69f51b4121d04510ffad7a4f374b3676afc3f8c
ThongAccount <thanhthongfitbit@gmail.com>
2025-04-13 21:33:17 +0700

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,11 @@
 from ultralytics import YOLO
 import torch
+import torch
+from torch.serialization import add_safe_globals
+from ultralytics.nn.tasks import DetectionModel
 
 # Load YOLOv5nu model (rất nhẹ)
+add_safe_globals([DetectionModel])
 model = YOLO("yolov5nu.pt")
 
 # Tải lại mô hình với weights_only=False để xử lý vấn đề unpickling
