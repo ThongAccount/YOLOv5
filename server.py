480d03949851f7a83fd15de928e98444e14ad401
ThongAccount <thanhthongfitbit@gmail.com>
2025-04-15 21:14:53 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from app import bp
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(bp)  # ← Đăng ký blueprint
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
