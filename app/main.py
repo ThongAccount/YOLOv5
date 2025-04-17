48be9a631504f2597a1c03ec21fd12bc8437a87e
ThongAccount <thanhthongfitbit@gmail.com>
2025-04-17 21:15:08 +0700

    a

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Blueprint, request, jsonify
 from app.utils import detect_objects
 import cv2
@@ -16,3 +17,14 @@ def detect():
 
     objects = detect_objects(image)
     return jsonify({"objects": objects})
+=======
+from flask import request, jsonify
+from app import bp
+from app.utils import detect_objects  # hoặc tên hàm bạn đặt
+
+@bp.route("/detect", methods=["POST"])
+def detect():
+    file = request.files["image"]
+    result = detect_objects(file)  # file là ảnh từ form
+    return jsonify({"objects": result})
+>>>>>>> parent of b2839bf (l)
