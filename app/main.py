b2839bf89c2d14841e512ff01c8ae1b90eb550bb
ThongAccount <thanhthongfitbit@gmail.com>
2025-04-17 20:49:59 +0700

    l

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
-from flask import request, jsonify
-from app import bp
-from app.utils import detect_objects  # hoặc tên hàm bạn đặt
+from flask import Flask, request, jsonify
+from flask import Flask
+from flask_cors import CORS
+from app.utils import detect_objects
 
-@bp.route("/detect", methods=["POST"])
-def detect():
-    file = request.files["image"]
-    result = detect_objects(file)  # file là ảnh từ form
-    return jsonify({"objects": result})
+app = Flask(__name__)
+CORS(app)  # Cho phép tất cả domain truy cập API
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    file = request.files['image']
+    results = detect_objects(file)
+    return jsonify({'objects': results})
--- app/utils.py
+++ app/utils.py
@@ -1,32 +1,14 @@
-from ultralytics import YOLO
+import torch
 from PIL import Image
-import os
+from io import BytesIO
 
-try:
-    model = YOLO("app/models/yolov5nu.pt")
-    print("✅ Model loaded OK!")
-except Exception as e:
-    print("❌ Error loading model:", e)
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='app/yolov5nu.pt', force_reload=True)
 
-def convert_image_to_png(original_path):
-    img = Image.open(original_path).convert("RGB")
-    new_path = os.path.splitext(original_path)[0] + ".png"
-    img.save(new_path, format="PNG")
-    return new_path
-
-def detect_objects(image_path):
-    # Chuyển ảnh về PNG
-    png_image_path = convert_image_to_png(image_path)
-
-    # Phát hiện đối tượng
-    results = model(png_image_path)
-
-    if not results:
-        return []
-
-    result = results[0]
-    names = model.names
-    detected_labels = result.boxes.cls.tolist()
-    unique_objects = list(set([names[int(i)] for i in detected_labels]))
-
-    return unique_objects
+def detect_objects(file):
+    img = Image.open(BytesIO(file.read()))
+    results = model(img)
+    labels = results.names
+    detected = results.pred[0][:, -1].tolist()
+    unique_labels = list(set([labels[int(i)] for i in detected]))
+    return unique_labels
+    
