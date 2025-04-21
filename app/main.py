78fd71013c280ac47730bf40ec24220ff272de45
ThongAccount <thanhthongfitbit@gmail.com>
2025-04-21 15:42:20 +0700

    a

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
+import time, logging
 from flask import Blueprint, request, jsonify
+from flask_cors import cross_origin
 from app.utils import detect_objects
 
 bp = Blueprint("main", __name__)
 
 @bp.route("/detect", methods=["POST"])
+@cross_origin()
 def detect():
+    start_time = time.time
+    logging.basicConfig(level=logging.DEBUG)
     if "image" not in request.files:
         return jsonify({"error": "No image uploaded"}), 400
 
@@ -14,4 +19,4 @@ def detect():
     objects = detect_objects(image_bytes)
 
     print(f"[📤 Response time] {time.time() - start_time:.2f}s")
-    return jsonify({"objects": results})
+    return jsonify({"objects": objects})
