a40bd7df6e7d83aa64c4e7c798c97ab58389d6ac
ThongAccount <thanhthongfitbit@gmail.com>
2025-04-21 14:54:27 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from flask_cors import CORS  # <- thêm dòng này
-from app import main
-
-bp = main.bp
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)  # <- bật CORS toàn cục
-    app.register_blueprint(bp)
+    CORS(app)  # <- Thêm dòng này
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
     return app
